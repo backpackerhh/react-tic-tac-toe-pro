88b23c729e6c4ebdbdf152d1d2c0835509bbee6e
David Montesdeoca <backpackerhh@gmail.com>
2020-02-24 20:32:07 +0100

    Add game states to allow user to choose player

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-import { DIMENSIONS, PLAYER_X, PLAYER_O, SQUARE_DIMENSIONS } from "./constants";
-import { getRandomInt } from "./utils";
+import { DIMENSIONS, PLAYER_X, PLAYER_O, SQUARE_DIMENSIONS, GAME_STATES } from "./constants";
+import { getRandomInt, switchPlayer } from "./utils";
 
 const Container = styled.div`
   display: flex;
@@ -29,14 +29,66 @@ const Marker = styled.p`
   font-size: 68px;
 `;
 
+const ButtonRow = styled.div`
+  display: flex;
+  width: 150px;
+  justify-content: space-between;
+`;
+
+const Screen = styled.div``;
+
+const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const ChooseText = styled.p``;
+
 const defaultBoard = new Array(DIMENSIONS ** 2).fill(null);
 
 const Game = () => {
   const [grid, setGrid] = useState(defaultBoard);
-  const [players, setPlayers] = useState({
-    human: PLAYER_X,
-    computer: PLAYER_O
-  });
+  const [players, setPlayers] = useState({ human: null, computer: null });
+  const [gameState, setGameState] = useState(GAME_STATES.notStarted);
+
+  const notStartedGame = () => {
+    return (
+      <Screen>
+        <Inner>
+          <ChooseText>Choose your player</ChooseText>
+          <ButtonRow>
+            <button onClick={() => choosePlayer(PLAYER_X)}>X</button>
+            <p>or</p>
+            <button onClick={() => choosePlayer(PLAYER_O)}>O</button>
+          </ButtonRow>
+        </Inner>
+      </Screen>
+    );
+  };
+
+  const alreadyStartedGame = () => {
+    return (
+      <Container dimensions={DIMENSIONS}>
+        {grid.map((value, index) => {
+          const isActive = value !== null;
+
+          return (
+            <Square key={index} onClick={() => humanMove(index)}>
+              {isActive && <Marker>{value === PLAYER_X ? "X" : "O"}</Marker>}
+            </Square>
+          );
+        })}
+      </Container>
+    );
+  };
+
+  const choosePlayer = option => {
+    setPlayers({ human: option, computer: switchPlayer(option) });
+
+    setGameState(GAME_STATES.inProgress);
+  };
 
   const move = (index, player) => {
     setGrid(grid => {
@@ -65,19 +117,7 @@ const Game = () => {
     }
   };
 
-  return (
-    <Container dimensions={DIMENSIONS}>
-      {grid.map((value, index) => {
-        const isActive = value !== null;
-
-        return (
-          <Square key={index} onClick={() => humanMove(index)}>
-            {isActive && <Marker>{value === PLAYER_X ? "X" : "O"}</Marker>}
-          </Square>
-        );
-      })}
-    </Container>
-  );
+  return gameState === GAME_STATES.notStarted ? notStartedGame() : alreadyStartedGame();
 };
 
 export default Game;
