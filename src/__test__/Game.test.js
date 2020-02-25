71ef753e969ca863ebfaa525605ef86d9b7672c2
David Montesdeoca <backpackerhh@gmail.com>
2020-02-25 12:48:03 +0100

    Add test to check that user is not able to move to a non-empty square

--- src/__test__/Game.test.js
+++ src/__test__/Game.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render, fireEvent, cleanup, waitForElement } from "@testing-library/react";
 
 import Game from "../components/Game";
+import { PLAYER_X, PLAYER_O } from "../components/constants";
 
 afterEach(cleanup);
 
@@ -24,4 +25,19 @@ it("should register and display result of human player's move", async () => {
   await waitForElement(() => getByText("O")); // Wait for computer move
 
   expect(getByText("O")).toBeInTheDocument(); // Check that we have 'O' in the DOM
+});
+
+it("should not make a move if the square is not empty", () => {
+  // prettier-ignore
+  const grid = [
+    PLAYER_X, null, PLAYER_O,
+    null, null, null,
+    null, null, null
+  ]
+  const { getByTestId, getByText } = render(<Game defaultGrid={grid} />);
+
+  fireEvent.click(getByText("X"));
+  fireEvent.click(getByTestId("square_2")); // Click non-empty square
+
+  expect(getByTestId("square_2")).toHaveTextContent("O"); // Should have initial value
 });
